f6531a76bf19e895915ebcdb2a31efefd369ca3b
Camilla Passer Hvidman <92184700+Vagarious@users.noreply.github.com>
2023-05-16 22:38:39 +0200

    Major upgrade in routing and introduction of hashed passwords
    Passwords are no longer stored in the database as plaintext.
    Routing now utilise the different methods more efficiently

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, flash, render_template, request, session, redirect, url_for
+from flask_session import Session
+from werkzeug.security import generate_password_hash, check_password_hash
 import psycopg2
 
 # Create a Flask app
 app = Flask(__name__)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Connect to the database using psycopg2 library and the database credentials
 conn = psycopg2.connect(
@@ -16,60 +22,59 @@ conn = psycopg2.connect(
 
 # Root route
 
-
-
 @app.route("/")
 def index():
-	if 'username' in session:
-		return render_template("signed_in.html", name=session["username"])
 	return render_template("index.html")
 
-
-@app.route('/created', methods=['POST'])
-def created():
-	session['username'] = request.form['firstname']
-	firstname=request.form["firstname"]
-	lastname=request.form["lastname"]
-	address=request.form["address"]
-	zipcode=request.form["zipcode"]
-	city=request.form['city']
-	email=request.form["email"]
-	password=request.form["password"]
-
-	cur = conn.cursor()
-	cur.execute(
-		"INSERT INTO users (firstname, lastname, address, zipcode, city, email, password) VALUES (%s, %s, %s, %s, %s, %s, %s)", (firstname, lastname, address, zipcode, city, email, password)
-	)
-	conn.commit()
-	cur.close()
-
-	return redirect(url_for('index'))
-
-@app.route('/signup', methods=['POST'])
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
+	if request.method == "POST":
+		print(request.form["password"])
+		firstname = request.form["firstname"]
+		lastname = request.form["lastname"]
+		address = request.form["address"]
+		zipcode = request.form["zipcode"]
+		city = request.form["city"]
+		email = request.form["email"]
+		_hashedPassword = generate_password_hash(request.form["password"])
+		print(_hashedPassword)
+
+		cur = conn.cursor()
+		cur.execute(
+			"""
+			INSERT INTO users (firstname, lastname, address, zipcode, city, email, password)
+			VALUES (%s, %s, %s, %s, %s, %s, %s)""",
+			(firstname, lastname, address, zipcode, city, email, _hashedPassword)
+		)
+		conn.commit()
+		return redirect("/")
 	return render_template("signup.html")
 
-@app.route('/login', methods=["POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
-	return render_template("login.html")
+	if request.method == "POST":
+		print(request.form["password"])
+		_hashedPassword = generate_password_hash(request.form["password"])
+		print(_hashedPassword)
+		cur = conn.cursor()
+		cur.execute(
+			""" SELECT email, firstname, password FROM users
+			WHERE email = '{}';
+			""".format(request.form["username"], _hashedPassword)
+		)
+		if cur.rowcount > 0:
+			user = cur.fetchone()
+			cur.close()
+			if check_password_hash(user[2], request.form["password"]):
+				session["username"] = user[0]
+				session["name"] = user[1]
+				return redirect("/")
+		else:
+			cur.close()
+			return render_template("error.html")
 
-@app.route('/signed_in', methods=["POST"])
-def signed_in(name=None):
-	print("username: ", request.form['username'])
-	print("password: ", request.form['password'])
-	cur=conn.cursor()
-	cur.execute(
-		""" SELECT * FROM users
-		WHERE email = '{}' AND
-		password = '{}';
-		""".format(request.form['username'], request.form['password'])
-	)
-	if cur.rowcount > 0:
-		result=cur.fetchone()
-		nametest = result[0]
-		print("nametest: ",nametest)
-		return render_template("signed_in.html", name=nametest)
-	return "Error"
+			
+	return render_template("login.html")
 
 if __name__ == '__main__':
 	app.debug=True
